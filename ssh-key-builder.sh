39ab9f72620c8346f0c3bdaf5874343b47c1a274
Chris Trimble <chris.trimble3.ct@gmail.com>
2023-12-03 23:29:21 -0500

    Syntax Updates
    Edit code syntax for improved hygiene, and prevent user inputs from escaping.

--- ssh-key-builder.sh
+++ ssh-key-builder.sh
@@ -11,24 +11,24 @@ sleep 2
 
 ##Create the keys, defining key as variable for script to automate import
 echo "Creating key..."
-read -p "Enter the name of your new key:" filename
+read -r -p "Enter the name of your new key:" filename
 
 ##Makes sure key is built in correct folder
-ssh-keygen -f ~/.ssh/${filename}
+ssh-keygen -f ~/.ssh/"${filename}"
 
 ##Define login string as variable to work with script
 echo "Now that you have created a key it can be imported to the server."
 
-read -p "Enter the username to use in SSH (like dietpi):" username
-read -p "Enter the server IP Address (like 192.168.101.201):" IP
+read -r -p "Enter the username to use in SSH (like dietpi):" username
+read -r -p "Enter the server IP Address (like 192.168.101.201):" IP
 
 echo "The default port for SSH is 22."
 
 ##Added support for custom SSH ports
 while true; do
-    read -p "Are you using a port different than 22 on your server? [Y/N] " yn
+    read -r -p "Are you using a port different than 22 on your server? [Y/N] " yn
         case $yn in
-        [Yy]* ) read -p "Enter your port here:" port && break;;
+        [Yy]* ) read -r -p "Enter your port here:" port && break;;
         [Nn]* ) echo "Okay, we are using port 22 then." && port="22" && break ;;
         * ) echo "If you are unsure, you probably use port 22." ;;
     esac
@@ -39,17 +39,17 @@ identity="${username}@$IP"
 ##Added support for Dropbear servers
 ##OpenSSH uses ssh-copy-id, Dropbear is manual since ssh-copy-id on Dropbear uses OpenWRT defaults
 while true; do
-    read -p "Is your server using OpenSSH or Dropbear? [O/D] " od
+    read -r -p "Is your server using OpenSSH or Dropbear? [O/D] " od
         case $od in
         [Oo]* ) 
                 echo "Copying your key to the server..."
                 ##Copies key with set parameters
-                ssh-copy-id -i ~/.ssh/${filename}.pub -p $port $identity
+                ssh-copy-id -i ~/.ssh/"${filename}".pub -p "$port" "$identity"
                 echo "Done."
 
                 ##Extra check for correct permissions, creates extra login prompt but worth it...
                 echo "Configuring host server..."
-                ssh -p $port $identity "sudo chmod 700 ~/.ssh; sudo chmod 600 ~/.ssh/authorized_keys; sudo chown ${username}:${username} ~/.ssh/authorized_keys"
+                ssh -p "$port" "$identity" "sudo chmod 700 ~/.ssh; sudo chmod 600 ~/.ssh/authorized_keys; sudo chown ${username}:${username} ~/.ssh/authorized_keys"
                 echo "Done."
 
                 ##Creates/adds to client ssh config file so key is correctly used
@@ -66,10 +66,10 @@ while true; do
                 ##Thanks to Dietpi's Joulinar & trendy for support
 
                 ##Move public key to Dropbear server
-                ssh -p $port $identity "mkdir -p ~/.ssh; tee -a ~/.ssh/authorized_keys" < ~/.ssh/${filename}.pub
+                ssh -p "$port" "$identity" "mkdir -p ~/.ssh; tee -a ~/.ssh/authorized_keys" < ~/.ssh/"${filename}".pub
 
                 ##Set the correct permissions for Dropbear server to accept keys as valid
-                ssh -p $port $identity "sudo chmod 700 ~/.ssh; sudo chmod 600 ~/.ssh/authorized_keys; sudo chown ${username}:${username} ~/.ssh/authorized_keys"
+                ssh -p "$port" "$identity" "sudo chmod 700 ~/.ssh; sudo chmod 600 ~/.ssh/authorized_keys; sudo chown ${username}:${username} ~/.ssh/authorized_keys"
 
                 ##Make config file so OpenSSH client can use correct key for Dropbear server
                 echo "Host ${IP}">>~/.ssh/config
@@ -85,9 +85,9 @@ done
 
 ##Ask user if new session is desired
 while true; do
-    read -p "Do you want to start an SSH session with your new key? [Y/N]" yn
+    read -r -p "Do you want to start an SSH session with your new key? [Y/N]" yn
         case $yn in
-        [Yy]* ) echo "Logging in..." && ssh -p $port $identity && break ;;
+        [Yy]* ) echo "Logging in..." && ssh -p "$port" "$identity" && break ;;
         [Nn]* ) echo "Okay, not logging in now." && break;;
         * ) echo 'Yes or No?' ;;
     esac
